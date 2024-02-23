04a5ab8accc0a2483df5088359071a9d8a6c02e8
DaniMendess <danimendes9728@gmail.com>
2024-02-23 14:17:41 -0300

    terceira tentativa de deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { BrowserRouter, Routes, Route,Navigate  } from 'react-router-dom'
-
 import './App.css'
-
 import Navbar from './components/Navbar'
 import Themes from './components/Themes'
 import Home from './pages/home/Home'
 import About from './pages/about/About'
 import Portifolio from './pages/portifolio/Portifolio'
-import Contact from './pages/Contact/Contact'
+import Contact from './pages/contact/Contact'
  
-
 function App() {
 
   return (
